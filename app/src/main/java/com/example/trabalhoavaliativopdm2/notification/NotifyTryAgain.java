724efc04e6210b8f9a7e907e844b4a48dc5ea0fd
wande <wande.leite@estudante.ifto.edu.br>
2022-06-18 23:06:52 -0300

    Função notificar implementada.

---
+++ app/src/main/java/com/example/trabalhoavaliativopdm2/notification/NotifyTryAgain.java
@@ -0,0 +1,41 @@
+package com.example.trabalhoavaliativopdm2.notification;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import com.example.trabalhoavaliativopdm2.R;
+import com.example.trabalhoavaliativopdm2.SplashSreenActivity;
+import com.example.trabalhoavaliativopdm2.pokemon.PokemonsData;
+
+public class NotifyTryAgain extends MyNotification {
+
+    private final static int NOTIFICACAO_ID = 5;
+
+    public NotifyTryAgain(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    void notifyNow() {
+        Intent intent = new Intent(context, SplashSreenActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pi = PendingIntent.getActivity(context, 0, intent, 0);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_pokeboll);
+
+        Notification builder = new NotificationCompat.Builder(context, CANAL_ID).setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Pokemon")
+                .setContentText("Vamos! tente novamente.")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pi)
+                .setLargeIcon(bitmap)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("Vamos fazer melhor, clicke aqui para jogar novamente.")).build();
+        NotificationManagerCompat nm = NotificationManagerCompat.from(context);
+        nm.notify(NOTIFICACAO_ID,builder);
+    }
+}
